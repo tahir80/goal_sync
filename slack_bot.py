538ec042fc3e23ec12266660e4db3778aa395674
localadmin <tahir.abbas.80@gmail.com>
2023-07-18 07:16:07 +0200

    make it better

--- slack_bot.py
+++ slack_bot.py
@@ -62,7 +62,8 @@ def interactivity():
     for action in actions:
         if action.get('text', {}).get('text') == "I want to create a goal first":
             print("User wants to create a goal first!")
-            session['goal_set'] = "goal_set"
+            session["goal_set"] = "goal_set"
+            print(session["goal_set"])
             # chatbot = SmartGoalSettingChatbot()
             # chatbot.start_conversation()
             # You can perform additional actions here based on the user's choice
@@ -80,7 +81,7 @@ def message(payload):
 
     if text == "hi":
         client.chat_postMessage(channel=channel_id, text='Hello World!')
-    if session['goal_set']:
+    if session["goal_set"]:
         print("success!")
 
 
