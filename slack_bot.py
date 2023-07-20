f0b0414566f4a835fa0055d9ce01428bb4027d7d
localadmin <tahir.abbas.80@gmail.com>
2023-07-20 16:58:24 +0200

    make it better

--- slack_bot.py
+++ slack_bot.py
@@ -107,7 +107,7 @@ def get_bot_id():
 BOT_ID = get_bot_id()
 
 @slack_event_adapter.on('message')
-def message(payload):jhgh
+def message(payload):
     print(payload)
     session.get('goal_set', 'not set')
     event = payload.get('event', {})
@@ -121,11 +121,15 @@ def message(payload):jhgh
 
     r = redis.Redis(connection_pool=REDIS_POOL)
     
-    if r.get('goal_set') is not None and user_id != BOT_ID:
-        message = chatbot.get_next_predict(text)
-        client.chat_postMessage(channel=channel_id, text=message)
-        if text.lower() == "exit" or text.lower() == "end":
-            print("Conversation ended. Goodbye!")
+    print("bot id is "+ BOT_ID)
+    print("user id is "+ user_id)
+
+    if r.exists('goal_set') and user_id != BOT_ID:
+        print("I was called from the combined logical conditions")
+        # message = chatbot.get_next_predict(text)
+        # client.chat_postMessage(channel=channel_id, text=message)
+        # if text.lower() == "exit" or text.lower() == "end":
+        #     print("Conversation ended. Goodbye!")
 
 
 
