302c042fde833e1585fb1a75aba915c9ac51fad7
localadmin <tahir.abbas.80@gmail.com>
2023-08-02 00:22:26 +0200

    make it better

--- Ingestion.py
+++ Ingestion.py
@@ -2,6 +2,9 @@ import os
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.pinecone import Pinecone
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
 
 # Initialize Pinecone
 pinecone.init(
@@ -23,14 +26,22 @@ async def ingest_docs(index_name, chat_history) -> None:
     embeddings = OpenAIEmbeddings()
     # vectorstore = Pinecone(index_name=index_name)
 
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100, chunk_overlap=20, separators=["\n\n", "\n", " ", ""]
+    )
+
+    documents = text_splitter.split_documents(documents=chat_history)
+
+
+
     # Check if the index is empty or not
     index = pinecone.Index(index_name)
     index_stats = index.describe_index_stats()
     print(index_stats)
     if index_stats['total_vector_count'] == 0:
         # Index is empty, add the chat history
-        Pinecone.from_documents(chat_history, embeddings, index_name=index_name)
+        Pinecone.from_documents(documents, embeddings, index_name=index_name)
     else:
         # Index is not empty, update the embeddings (upsert operation)
-        Pinecone.upsert_documents(chat_history, embeddings, index_name=index_name)
+        Pinecone.upsert_documents(documents, embeddings, index_name=index_name)
     
